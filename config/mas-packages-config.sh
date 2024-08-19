1ebbfbfd8ee78f6b6efecac439b99460837486dd
Guido X Jansen <x@gui.do>
2024-08-19 23:23:30 +0200

    Update mas-packages-config.sh
    Updated with my appstore apps, removed the ones that can be installed with homebrew

--- config/mas-packages-config.sh
+++ config/mas-packages-config.sh
@@ -2,18 +2,15 @@
 
 # Common Mac App Store packages for all scenarios
 COMMON_MAS_PACKAGES=(
-    "497799835::Xcode::"
-    "409183694::Keynote::"
-    "409201541::Pages::"
-    "409203825::Numbers::"
+    "561350520::GoPro Quik::"
+    "1246969117::Steam Link::"
+    "545519333::Prime Video::"
 )
 
 # Desktop-only Mac App Store packages
 DESKTOP_MAS_PACKAGES=(
-    "1295203466::Microsoft Remote Desktop::"
 )
 
 # Laptop-only Mac App Store packages
 LAPTOP_MAS_PACKAGES=(
-    "775737590::iA Writer::"
 )
