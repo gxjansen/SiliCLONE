2ec6577e7280e1e1c99fde9426a44362e9ad570b
Guido X Jansen <x@gui.do>
2024-08-19 16:46:08 +0200

    Create user_settings.sh

---
+++ config/user_settings.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# User-specific settings
+USER_EMAIL="your_email@example.com"
+GITHUB_USERNAME="your_github_username"
+
+# Add any other user-specific settings here
+# For example:
+# PREFERRED_SHELL="/bin/zsh"
+# DEFAULT_BROWSER="Safari"
