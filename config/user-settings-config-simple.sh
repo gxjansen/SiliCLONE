752339f32a2eac7c6ebe07bcfe340d4612136f8a
Guido X Jansen <x@gui.do>
2024-08-20 23:16:39 +0200

    Update user-settings-config-simple.sh

--- config/user-settings-config-simple.sh
+++ config/user-settings-config-simple.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 # User-specific settings
-USER_EMAIL="your_email@example.com"
-GITHUB_USERNAME="your_github_username"
-
-# Installation scenario
-# Set to either "desktop" or "laptop"
-INSTALLATION_SCENARIO="desktop"
+USER_EMAIL="x@gui.do"
+GITHUB_USERNAME="gxjansen"
