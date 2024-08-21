4e2fbc589a734fd16587c77c895e418f40580310
Guido X Jansen <x@gui.do>
2024-08-21 15:57:04 +0200

    Delete config/user_settings.sh

--- config/user_settings.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# User-specific settings, replace with your own email and username!
-USER_EMAIL="x@gui.do"
-GITHUB_USERNAME="gxjansen"
-
-# You can add any other user-specific settings here
-# For example:
-# PREFERRED_SHELL="/bin/zsh"
